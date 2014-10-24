0725b86d0226ceda8f223747bf3e863746fffbb9
Mike Piccolo <mfpiccolo@gmail.com>
2014-10-23 22:39:08 -0700

    Use jruby stemmer

--- lib/classifier-reborn/category_namer.rb
+++ lib/classifier-reborn/category_namer.rb
@@ -2,14 +2,15 @@
 # Copyright:: Copyright (c) 2005 Lucas Carlson
 # License::   LGPL
 
-require 'fast_stemmer'
+# require 'fast_stemmer'
+require "jruby-stemmer"
 require 'classifier-reborn/extensions/hasher'
 
 module ClassifierReborn
   module CategoryNamer
     extend self
-    def prepare_name(name) 
-      name.to_s.gsub("_"," ").capitalize.intern 
+    def prepare_name(name)
+      name.to_s.gsub("_"," ").split.map(&:capitalize).join(' ').intern
     end
   end
 end
